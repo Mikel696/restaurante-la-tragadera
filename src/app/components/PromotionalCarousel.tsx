8b0910c67063219c4afd58ce6e0710d95d888976
MiguelBarros <migueiro69@hotmail.com>
2025-09-23 13:30:47 -0500

    feat(carousel): add banner-rotativo2.jpg to promotional carousel

--- src/app/components/PromotionalCarousel.tsx
+++ src/app/components/PromotionalCarousel.tsx
@@ -14,6 +14,7 @@ import Autoplay from 'embla-carousel-autoplay';
 const promotionalImages = [
   '/images/banner-largo-horizontal-promo.jpg',
   '/images/banner-promocion.jpg',
+  '/images/banner-rotativo2.jpg',
 ];
 
 const PromotionalCarousel = () => {
